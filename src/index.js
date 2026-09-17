664dffdfe5f4d999a62c0dd156ff37bf551ed59c
unknown <213911056+Pasindujeewan@users.noreply.github.com>
2026-09-17 22:11:50 +0530

    feat:add mongodb connection and configure and test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import app from "./app.js";
 
-const app = express();
+import { connectDB } from "./config/db.js";
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
@@ -10,42 +10,8 @@ const io = new Server(httpServer, {
     origin: "*",
   },
 });
-io.on("connection", (socket) => {
-  console.log("a user connected");
 
-  socket.on("message", (message) => {
-    console.log("message from user", socket.id, "message is", message);
-  });
-
-  socket.emit("message", "hello from server");
-
-  socket.on("joinConversation", (data) => {
-    console.log("joinConversation data", data);
-    const { userId, otherUserId } = JSON.parse(data);
-    const roomId = [userId, otherUserId].sort().join("_");
-
-    socket.join(roomId);
-
-    console.log(`User ${userId} joined room ${roomId}`);
-  });
-
-  socket.on("sendMessage", (data) => {
-    console.log("sendMessage data", data);
-    const { senderId, receiverId, text } = JSON.parse(data);
-    const roomId = [senderId, receiverId].sort().join("_");
-
-    console.log(`User ${senderId} sent message to room ${roomId}: ${text}`);
-
-    io.to(roomId).emit("receiveMessage", {
-      senderId,
-      text,
-    });
-  });
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-});
+await connectDB();
 
 httpServer.listen(3000, () => {
   console.log("listening on *:3000");
