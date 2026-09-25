7c6a76ae7ad8be9704d0808b1d9daa17d714b312
unknown <213911056+Pasindujeewan@users.noreply.github.com>
2026-09-25 19:02:43 +0530

    feat:add message and covercation model

--- src/middleware/verifyAccessToken.js
+++ src/middleware/verifyAccessToken.js
@@ -5,6 +5,7 @@ import User from "../models/User.js";
 export const verifyAccessTokenMiddleware = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
+    console.log(authHeader + "");
 
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       throw new ApiError("Access token required", 401, "UNAUTHORIZED");
--- src/sockets/chat.socket.js
+++ src/sockets/chat.socket.js
@@ -4,6 +4,7 @@ export const registerChatEvents = (io, socket) => {
   socket.join(`user:${userId}`);
 
   socket.on("sendMessage", async (data) => {
+    console.log(data);
     const { receiverId, content } = data;
     io.to(`user:${receiverId}`).emit("newMessage", {
       senderId: userId,
